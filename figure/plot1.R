2ef7f75bf1a2329be0b47d9e24c34d282bd57fc0
karthikeyan Ramakrishnan <karthikeyan.ramakrishnan@aol.com>
2014-06-08 15:06:23 +0530

    regualrised

--- figure/plot1.R
+++
@@ -1,6 +0,0 @@
-x<-read.csv("household_power_consumption.txt",nrow=85000,sep=";")
-y<-x[(x$Date=="1/2/2007"|x$Date=="2/2/2007"),]
-z<-as.numeric(as.character(y[,3]))
-hist(z,main="Global Active Power",xlab="Global Active Power(kilowatts)",col="red")
-dev.copy(png,file="plot1.png")
-dev.off()
--- figure/plot2.R
+++
@@ -1,9 +0,0 @@
-x<-read.csv("household_power_consumption.txt",nrow=85000,sep=";")
-y<-x[(x$Date=="1/2/2007"|x$Date=="2/2/2007"),]
-a<-as.Date(y$Date,format="%d/%m/%Y")
-b<-as.character(y$Time)
-c<-paste(a,b)
-d<-strptime(c,format="%Y-%m-%d %H:%M:%S")
-plot(d,as.numeric(as.character(y[,3])),type="l",xlab="",ylab="Global Active Power(kilowatts)")
-dev.copy(png,file="plot2.png")
-dev.off()
